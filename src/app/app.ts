4197a8b9b0d55255a7afa500c0cc8af3745edf1c
Yuliia <marinina.yuliia@gmail.com>
2026-02-15 18:43:05 +0100

    chore: rename promo-section.component folder to promo-section

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { ButtonComponent } from './shared/components/button/button.component';
 import { FloatingButtonsComponent } from './shared/components/floating-buttons.component/floating-buttons.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { PromoSectionComponent } from './shared/components/promo-section.component/promo-section.component';
+import { PromoSectionComponent } from './shared/components/promo-section/promo-section.component';
 
 @Component({
   selector: 'app-root',
