7269646d575c50e244b24606391d9b31487f7dda
Yuliia <marinina.yuliia@gmail.com>
2026-02-15 01:31:44 +0100

    feat: create Footer component with navigation and social links

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import {
+  LucideAngularModule,
+  MessageCircleCode,
+  MessageSquareCode,
+  SmilePlus,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class Footer {}
+export class FooterComponent {
+  readonly MessageSquareCode = MessageSquareCode;
+  readonly MessageCircleCode = MessageCircleCode;
+  readonly SmilePlus = SmilePlus;
+
+  cartItemsCount = 3; // Example cart item count
+}
